384283ac4c39918558de006ebe8019c6bc38a7f1
Everton cadona <cadonaenrike@gmail.com>
2023-11-07 19:46:06 -0300

    coreção e alteração do home

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,19 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import CardTwittes from '../components/CardsTwittes';
 import DefaultLayout from '../config/layout/DefaultLayot';
-import { redirect } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
 const Home: React.FC = () => {
   const userId = localStorage.getItem('usuarioId');
   const session = sessionStorage.getItem('token');
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (userId === null && session === null) {
+      navigate('/login');
+    }
+  }, []);
+
   if (userId != null && session != null) {
     return (
       <DefaultLayout>
@@ -13,8 +21,6 @@ const Home: React.FC = () => {
         <CardTwittes userId={null} />
       </DefaultLayout>
     );
-  } else {
-    redirect('/login');
   }
 };
 
