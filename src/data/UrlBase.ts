b762593e1caaa8baf5ef97aa39956d4f8f5c8ce9
Everton cadona <cadonaenrike@gmail.com>
2023-11-08 23:37:45 -0300

    ajuste dos data

--- src/data/UrlBase.ts
+++ src/data/UrlBase.ts
@@ -3,8 +3,8 @@ import axios from 'axios';
 const API_BASE_URL = 'https://api-growtwitter.onrender.com';
 export default API_BASE_URL;
 
-// https://api-growtwitter.onrender.com       api no ar
-//http://localhost:3333                       servidor de teste local
+// https://api-growtwitter.onrender.com       api do render
+//http://localhost:3333                       servidor de teste localhosts 5173
 
 export const API = axios.create({
   baseURL: API_BASE_URL,
